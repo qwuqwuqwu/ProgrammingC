3470785f834ee596724ea20b39e6a715f6c49867
qwuqwuqwu <qwuqwuqwu@gmail.com>
2022-04-16 11:37:34 +0800

    add: 1. E09_03 input file 2. PrefixStringToInfixString fuction

--- ClassExample/W09/E09_03.c
+++ ClassExample/W09/E09_03.c
@@ -1,33 +1,83 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int cal(void)
-{
-  int ans, op1, op2;
-  char c = getchar();
-  if (c == ' ') {
-		return cal();
-	} else if (isdigit(c)) {
-    ungetc(c, stdin);
-    scanf("%d", &ans);
-    return ans;
-  } else if(c == '+') {
-  	op1 = cal();
-    op2 = cal();
+int Prefix( void )
+{   
+    int ans, op1, op2;
+    char c = getchar();
+    if( c == ' ' ) {
+	    return Prefix();
+    }
+    else if ( isdigit( c ) ) {
+        ungetc(c, stdin);
+        scanf("%d", &ans);
+        return ans;
+    }
+    
+    // + is leading two number, so this is a prefix
+    if( c == '+' ) {
+        op1 = Prefix();
+        op2 = Prefix();
 		return op1 + op2;
-	} else if(c == '-') {
-  	op1 = cal();
-    op2 = cal();  
+	}
+    else if( c == '-' ) {
+        op1 = Prefix();
+        op2 = Prefix();  
 		return op1 - op2;
-	} else if(c == '*') {
-  	op1 = cal();
-    op2 = cal();
+	}
+    else if( c == '*' ) {
+        op1 = Prefix();
+        op2 = Prefix();
 		return op1 * op2;
 	}
+    return 0;
+}
+
+int PrefixStringToInfixString()
+{
+    int ans, op1, op2;
+    char c = getchar();
+    if( c == ' ' ) {
+	    return PrefixStringToInfixString();
+    }
+    else if ( isdigit( c ) ) {
+        ungetc(c, stdin);
+        scanf("%d", &ans);
+        printf( "%d", ans );
+        return ans;
+    }
+    
+    // + is leading two number, so this is a prefix
+    if( c == '+' ) {
+        printf( " ( " );
+        op1 = PrefixStringToInfixString();
+        printf( " + " );
+        op2 = PrefixStringToInfixString();
+        printf( " ) " );
+		return op1 + op2;
+	}
+    else if( c == '-' ) {
+        printf( " ( " );
+        op1 = PrefixStringToInfixString();
+        printf( " - " );
+        op2 = PrefixStringToInfixString();  
+        printf( " ) " );
+		return op1 - op2;
+	}
+    else if( c == '*' ) {
+        printf( " ( " );
+        op1 = PrefixStringToInfixString();
+        printf( " * " );
+        op2 = PrefixStringToInfixString();
+        printf( " ) " );
+		return op1 * op2;
+	}
+    return 0;
 }
 
 int main(void)
 {
-  printf("%d\n", cal());
-  return 0;
+    //printf("%d\n", Prefix());
+    printf(" = %d\n", PrefixStringToInfixString());
+    return 0;
 }
