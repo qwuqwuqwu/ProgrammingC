56960b7d36366417dfa6d386fdcca633989ae29c
qwuqwuqwu <qwuqwuqwu@gmail.com>
2022-04-16 10:53:26 +0800

    mod: add week folder

---
+++ ClassExample/W09/E09_01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void binary( unsigned int i )
+{
+    if (i > 0) {
+        binary(i/2); 
+        printf("%u", i%2);
+    }
+}
+
+void Hexadecimal( unsigned int i )
+{
+    if( i > 0 ) {
+        Hexadecimal( i / 16 );
+        if( i % 16 >= 10 ) {
+            printf( "%u", i / 16 - 'A' );
+        }
+        else {
+            printf( "%u", i / 16 );
+        }
+    }
+}
+
+int main( void )
+{
+    unsigned int n;
+    scanf( "%u", &n );
+    binary( n );
+    return 0;
+}
