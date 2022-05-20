f536a7816a07edd0e68f1d740f10f10bbae97416
10101389 <chunsheng.chen@syntecclub.com.tw>
2022-05-20 13:15:19 +0800

    mod: add HW10_A, HW10_B

---
+++ HW10_B.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float x;
+
+int main( void )
+{
+    
+    int* a = ( int* )&x;
+    for( int i = 1; i N(); i*=2 ) {
+        if( a / i ) 
+    }
+
+    printf( "%d", *a );
+    return 0;
+}
